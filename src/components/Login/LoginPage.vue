9f23be20a8a4acdcaba0a0f1bb617718b821d2e6
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-03-21 08:03:27 -0300

    Create Login page

---
+++ src/components/Login/LoginPage.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <vue-container fluid>
+            <v-row justify="center">
+                <v-col cols="12" md="4" lg="3" class="text-center">
+                    <h6 class="headline">Login</h6>
+                    <v-text-field label="Token" v-model="token"></v-text-field>
+
+                    <v-btn depresed
+                           dark color="green"
+                           block
+                           @click="login">
+                        Login
+                    </v-btn>
+                    <v-btn text
+                           @click="loginGuest">Entrar como visitante
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </vue-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LoginPage',
+        data() {
+            return {
+                token: '',
+            };
+        },
+        methods: {
+            login() {
+                alert(this.token);
+            },
+            loginGuest() {
+
+            },
+        },
+    };
+</script>
+
+<style scoped>
+
+</style>
