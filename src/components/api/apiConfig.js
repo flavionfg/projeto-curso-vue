245fb20b8cb40452a811f0e9585a99e7ecb0d61a
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-03-31 19:44:39 -0300

    create api validation method

--- src/components/api/apiConfig.js
+++ src/components/api/apiConfig.js
@@ -15,6 +15,12 @@ export default {
                 configuration.url += `key=${this.$store.state.authToken}`;
 
                 return configuration;
+            }, () => {
+                this.$store.commit('showErrorMessage', 'Ops" Houve um problema ao enviar a requsição');
+            });
+
+            apiInstance.interceptors.response.use((response) => response, () => {
+                this.$store.commit('showErrorMessage', 'Ops! Houve um problema com a sua requisição. ');
             });
         },
     },
